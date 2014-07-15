9f7f055ec0a70460e851c188c65aa931b05788e0
lxndrtsh <lxndr.tsh@gmail.com>
2014-07-15 09:51:41 -0500

    DB Set
    DB info set.

--- src/app/config/database.php
+++ src/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'songtangle',
+			'username'  => 'root',
+			'password'  => 'Mk33pdt7g!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
