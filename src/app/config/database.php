9749b7b68be46c2f82717787f7ea31577eeeff34
bvotava-757 <bvotava@rxsavingsllsc.com>
2014-07-15 11:55:14 -0500

    Commit

--- src/app/config/database.php
+++ src/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'songtangle',
 			'username'  => 'root',
-			'password'  => 'Mk33pdt7g!',
+			'password'  => 'ratamacue',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
